c57add5e2410067ce7cc65b009647627cc176c0f
Abdelrhman Nasr <abdoo.nasr22@gmail.com>
2026-02-11 18:17:06 +0200

    resolved

--- Tas-9/src/com/hibernte/entity/user.java
+++ Tas-9/src/com/hibernte/entity/user.java
@@ -1,11 +1,15 @@
 package com.hibernte.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Setter
+@Getter
 @Entity
 @Table(name ="user")
 public class user {
@@ -30,37 +34,6 @@ public class user {
         this.username = username;
         this.email = email;
         this.password = password;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
