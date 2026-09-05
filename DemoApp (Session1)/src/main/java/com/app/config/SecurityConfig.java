49c1c865753f665d1f352eadf20fd453aa85b961
Abdelrhman Nasr <abdoo.nasr22@gmail.com>
2026-09-06 01:35:18 +0300

    Add security task 1

---
+++ DemoApp (Session1)/src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("ahmed").password("123").roles("MANAGER"))
+                .withUser(users.username("ash").password("123").roles("ADMIN"));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().
+                antMatchers("/homePage").hasRole("MANAGER")
+                .antMatchers("/adminPage").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .defaultSuccessUrl("/", true)
+                .permitAll();
+
+
+    }
+}
+
+
---
+++ DemoApp (Session1)/src/main/webapp/WEB-INF/view/homePage.jsp
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+<h1>Hello</h1>
+
+<form action="${pageContext.request.contextPath}/adminPage" method="get">
+    <button type="submit">Go to Admin Page</button>
+</form>
+
+</body>
+</html>
